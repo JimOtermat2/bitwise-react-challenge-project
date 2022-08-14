926ac4de0e0574c62ab919fa7129f07bc9c0a86a
Jim Otermat <jim.otermat@gmail.com>
2022-08-14 11:15:05 -0700

    feat(order-list): Live refresh
    Auto-update the View Orders page every 5 seconds
    
    Fixes #12

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -1,31 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
 import OrdersList from './ordersList';
 import './viewOrders.css';
 
-export default function ViewOrders(props) {
-    const [orders, setOrders] = useState([]);
+const CURRENT_ORDERS_URL = `${SERVER_IP}/api/current-orders`;
+const LIVE_ORDER_MODE_URL = `${SERVER_IP}/api/live-mode`;
 
-    useEffect(() => {
-        fetch(`${SERVER_IP}/api/current-orders`)
-            .then(response => response.json())
-            .then(response => {
-                if(response.success) {
-                    setOrders(response.orders);
-                } else {
-                    console.log('Error getting orders');
-                }
-            });
-    }, [])
+class ViewOrders extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: []
+        };
+    }
 
-    return (
-        <Template>
-            <div className="container-fluid">
-                <OrdersList
-                    orders={orders}
-                />
-            </div>
-        </Template>
-    );
+    interval;
+
+    // define our use functions
+    refreshOrders = () => {
+        fetch(CURRENT_ORDERS_URL)
+        .then(response => response.json())
+        .then(response => {
+            if(response.success) {
+                this.setState({orders: response.orders});
+            } else {
+                console.log('Error getting orders');
+            }
+        });
+    }
+
+    turnOnLiveMode = () => {
+        fetch(LIVE_ORDER_MODE_URL, {
+            method: `POST`
+        })
+    }
+
+    componentDidMount() {
+        // refresh after mounting
+        this.refreshOrders();
+        // clear interval just in case
+        clearInterval(this.interval);
+        // start regular refresh, every 5 seconds
+        this.interval = setInterval(this.refreshOrders, 5000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <Template>
+                <div className="container-fluid">
+                    <button className="btn btn-success" onClick={this.turnOnLiveMode}>Live Mode!</button>
+                    <OrdersList
+                        orders={this.state.orders}
+                    />
+                </div>
+            </Template>
+        );
+    }
 }
+
+export default ViewOrders;
