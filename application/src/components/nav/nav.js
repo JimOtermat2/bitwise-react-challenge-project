fa9bff34068e4535d7cc8be84183d378bb95e255
Jim Otermat <jim.otermat@gmail.com>
2022-08-13 17:38:07 -0700

    feat(nav-bar): Add display of user name to nav bar
    Grab the user's email from the state, and display it in the nav bar.
    
    Fixes #10

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,25 +1,32 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 import "./nav.css";
 
 const Nav = (props) => {
+    const auth = useSelector((state) => state.auth);
     return (
-        <div className="nav-strip">
-            <Link to={"/order"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Order Form</label>
-                </div>
-            </Link>
-            <Link to={"/view-orders"} className="nav-link" id="middle-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">View Orders</label>
-                </div>
-            </Link>
-            <Link to={"/login"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Log Out</label>
-                </div>
-            </Link>
+        <div>
+            <div className="nav-strip">
+                <label className="nav-label">Welcome Back, {auth.email}</label>
+            </div>
+            <div className="nav-strip">
+                <Link to={"/order"} className="nav-link">
+                    <div className="nav-link-style">
+                        <label className="nav-label">Order Form</label>
+                    </div>
+                </Link>
+                <Link to={"/view-orders"} className="nav-link" id="middle-link">
+                    <div className="nav-link-style">
+                        <label className="nav-label">View Orders</label>
+                    </div>
+                </Link>
+                <Link to={"/login"} className="nav-link">
+                    <div className="nav-link-style">
+                        <label className="nav-label">Log Out</label>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
